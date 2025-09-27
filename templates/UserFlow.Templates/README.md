5c8ebf9a42c3ddf5035b2e864838f07d4aeb0e53
Axel Kjønsberg <axelkjonsberg@gmail.com>
2025-09-27 14:38:43 +0200

    Clean up template

--- templates/UserFlow.Templates/README.md
+++ templates/UserFlow.Templates/README.md
@@ -1,7 +1,7 @@
 <!-- Provide an overview of what template package does and how to get started.
 Consider previewing the README before uploading (https://learn.microsoft.com/en-us/nuget/nuget-org/package-readme-on-nuget-org#preview-your-readme). -->
 
-> dotnet new install UserFlowFunctions.Templates::1.2.3
+> dotnet new install AxelKjonsberg.AzureUserFlow.Templates::1.2.3
 
 or, latest stable:
-> dotnet new install UserFlowFunctions.Templates
+> dotnet new install AxelKjonsberg.AzureUserFlow.Templates
--- templates/UserFlow.Templates/content/ApiConnector/src/Program.cs
+++ templates/UserFlow.Templates/content/ApiConnector/src/Program.cs
@@ -1,17 +1,15 @@
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Microsoft.Azure.Functions.Worker;
+
+namespace ProjectName;
 
 var builder = Host.CreateDefaultBuilder(args)
-  .ConfigureFunctionsWorkerDefaults(worker =>
-  {
-      worker.UseMiddleware<BasicAuthMiddleware>();
-  })
-  .ConfigureServices(services =>
-  {
-  })
-  .ConfigureLogging(lb => lb.AddFilter("Microsoft", LogLevel.Information));
+    .ConfigureFunctionsWorkerDefaults(worker =>
+    {
+        worker.UseMiddleware<BasicAuthMiddleware>();
+    })
+    .ConfigureServices(_ => { })
+    .ConfigureLogging(loggingBuilder => loggingBuilder.AddFilter("Microsoft", LogLevel.Information));
 
 var app = builder.Build();
 await app.RunAsync();
